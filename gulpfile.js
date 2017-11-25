9912f01f9d1632c043b71c7087cd8e66ee704758
Muslim <010.muslim@gmail.com>
2017-11-26 03:03:05 +0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,19 @@ var sass = require('gulp-sass');
 var minifier = require('gulp-cssnano');
 var rename = require('gulp-rename');
 
-var sassFiles = 'assets/scss/precompiled/*.scss',
-    cssDest = 'assets/css/',
-    cssFiles = 'assets/css/*.css',
-    cssMinDest = 'assets/css-min/';
+var sassFiles = 'assets/scss/**/*.scss',
+    preCompiledFiles = 'assets/scss/precompiled/*.scss',
+    cssDest = 'assets/css/';
 
-gulp.task('compile', function(){
-    gulp.src(sassFiles)
+gulp.task('compile', function () {
+    gulp.src(preCompiledFiles)
         .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest))
+        .pipe(minifier())
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('minify', function(){
-    gulp.src(cssFiles)
-        .pipe(minifier())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(cssMinDest));
-});
-
-gulp.task('watch', function(){
-    gulp.watch(sassFiles,['compile','minify']);
+gulp.task('watch', function () {
+    gulp.watch(sassFiles, ['compile']);
 });
